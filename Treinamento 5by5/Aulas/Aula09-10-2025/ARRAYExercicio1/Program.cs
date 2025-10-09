3c85b35d8d1689c2f9bf5733a35782a7561ab578
krysgh <krysthiang@gmail.com>
2025-10-09 16:06:19 -0300

    Inclusão do exercício de Array proposto em sala

---
+++ Treinamento 5by5/Aulas/Aula09-10-2025/ARRAYExercicio1/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int i = 0, tamanho = 0;
+
+Console.WriteLine("Digite o tamanho do Array: ");
+tamanho = Convert.ToInt32(Console.ReadLine());
+
+int[] numeros = new int[tamanho];
+
+
+for(i = 0; i < numeros.Length; i++)
+{
+    numeros[i] = Random.Shared.Next(0, 100);
+}
+
+Console.Write("\nVETOR: ");
+
+for(i = 0; i < numeros.Length; i++)
+{
+    Console.Write(numeros[i] + " ");
+}
+
+
+Console.Write("\n\nPRIMEIRA METADE DO VETOR: ");
+
+for (i = 0;i < numeros.Length/ 2; i++)
+{ 
+    Console.Write(numeros[i] + " ");
+}
+
+Console.Write("\n\nSEGUNDA METADE DO VETOR NA ORDEM INVERSA: ");
+
+for(i = numeros.Length - 1; i >= numeros.Length / 2; i--)
+{
+    Console.Write(numeros[i] + " ");
+}
+
+Console.WriteLine();
